756730aa58a723af7fab8c2fbb58c54708ca2b16
Eugene Kapombe <kapombek@gmail.com>
2025-05-28 06:27:34 +0300

    db running

---
+++ models.py
@@ -0,0 +1,52 @@
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, Date, Float, Text, ForeignKey
+from sqlalchemy.orm import relationship
+
+Base = declarative_base()
+
+class Patient(Base):
+    __tablename__ = 'patients'
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    age = Column(Integer, nullable=False)
+    phone_number = Column(Integer, nullable=False)
+    account_type = Column(String, nullable=False)
+    visits = relationship("Visit", back_populates="patient")
+    appointments = relationship("Appointments", back_populates="patient")
+
+class Doctor(Base):
+    __tablename__ = 'doctors'
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    visits = relationship("Visit", back_populates="doctor")
+    appointments = relationship("Appointments", back_populates="doctor")
+
+class Visit(Base):
+    __tablename__ = 'visits'
+    id = Column(Integer, primary_key=True)
+    date = Column(Date, nullable=False)
+    summary = Column(String, nullable=False)
+    procedure_details = Column(Text, nullable=False)
+    amount_paid = Column(Float, nullable=False)
+    balance = Column(Float)
+    doctor_id = Column(Integer, ForeignKey('doctors.id'))
+    patient_id = Column(Integer, ForeignKey('patients.id'))
+    patient = relationship("Patient", back_populates="visit")
+    doctor = relationship("Doctor", back_populates="visits")
+    prescription = relationship("Prescription", back_populates="visits")
+
+class Appointment(Base):
+    __tablename__ = 'appointments'
+    id = Column(Integer, primary_key=True)
+    date = Column(Date, nullable=False)
+    patient_id = Column(Integer, ForeignKey('patients.id'))
+    doctor_id = Column(Integer, ForeignKey('doctors.id'))
+    patient = relationship("Patient", back_populates="appointments")
+    doctor = relationship("Doctor", back_populates="appointments")
+
+class Prescription(Base):
+    __tablename__ = 'prescriptions'
+    id = Column(Integer, primary_key=True)
+    details = Column(Text, nullable=False)
+    visit_id = Column(Integer, ForeignKey('visits.id'))
+    visit = relationship("Visit", back_populates="prescriptions")
