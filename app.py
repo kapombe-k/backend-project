c45828f6affb7f04b44f2b0418ff5e7f3d6f422d
Eugene Kapombe <kapombek@gmail.com>
2025-05-29 12:44:45 +0300

    created schemas

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 #Import fastapi package
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from fastapi.middleware.cors import CORSMiddleware
+
+from models import get_db, Patient, Visit, Doctor, Appointment, Prescription
 
 #initialize it
 
 app = FastAPI()
+
+#this allows network requests from all servers
+app.add_middleware(CORSMiddleware, allow_origins=['*'])
 
 #define the routes
 @app.get('/')
@@ -11,10 +17,30 @@ def index():
     return{'message':'Up and running'}
 
 @app.get('/patients')
-def patients():
+def all_patients(session = Depends(get_db)):
+    #sqlalchemy retrieves all patients from the table
+    all_patients = session.query(Patient).all()
+    return all_patients
+
+# http://localhost:9000/products -> GETs a single patient
+@app.get('/patients/{id}')
+def get_patients(id:int):
+    print("Patient id", id)
     return []
 
+# http://localhost:9000/products -> POST
 @app.post('/patients')
 def add_patients():
     return {'message':'Patient added successfully'}
+
+@app.patch('/patients/{id}')
+def update_patient(id:int):
+    print(f'Patient {id} has been updated')
+    return {'message':'Patient added successfully'}
+
+@app.delete('/patients/{id}')
+def delete_patient(id:int):
+    print(f'Patient {id} has been deleted')
+    return {'message':'Patient deleted successfully'}
+
     
--- models.py
+++ models.py
@@ -1,6 +1,21 @@
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, Date, String, Float, Text, ForeignKey
+from sqlalchemy import Column, Integer, Date, String, Float, Text, ForeignKey, create_engine
 from sqlalchemy.orm import relationship
+from sqlalchemy.orm import sessionmaker
+
+#1. create the engine that connects the db
+engine = create_engine('sqlite:///clinic.db', echo=True)
+
+#2. create a session
+Session = sessionmaker(bind=engine)
+
+# 3. for fastapi, we need to create a method/ function that returns the session
+def get_db():
+    session = Session()
+    try:
+        yield session
+    finally:
+        session.close()
 
 
 Base = declarative_base()
@@ -14,14 +29,14 @@ class Patient(Base):
     address = Column(String, nullable=False)
     account_type = Column(String, nullable=False)
     visits = relationship("Visit", back_populates="patient")
-    appointments = relationship("Appointments", back_populates="patient")
+    appointments = relationship("Appointment", back_populates="patient")
 
 class Doctor(Base):
     __tablename__ = 'doctors'
     id = Column(Integer, primary_key=True)
     name = Column(String, nullable=False)
     visits = relationship("Visit", back_populates="doctor")
-    appointments = relationship("Appointments", back_populates="doctor")
+    appointments = relationship("Appointment", back_populates="doctor")
 
 class Visit(Base):
     __tablename__ = 'visits'
@@ -33,9 +48,9 @@ class Visit(Base):
     balance = Column(Float, nullable=True)
     doctor_id = Column(Integer, ForeignKey('doctors.id'))
     patient_id = Column(Integer, ForeignKey('patients.id'))
-    patient = relationship("Patient", back_populates="visit")
+    patient = relationship("Patient", back_populates="visits")
     doctor = relationship("Doctor", back_populates="visits")
-    prescription = relationship("Prescription", back_populates="visits")
+    prescription = relationship("Prescription", back_populates="visit")
 
 class Appointment(Base):
     __tablename__ = 'appointments'
@@ -52,3 +67,5 @@ class Prescription(Base):
     details = Column(Text, nullable=False)
     visit_id = Column(Integer, ForeignKey('visits.id'))
     visit = relationship("Visit", back_populates="prescriptions")
+
+Base.metadata.create_all(bind=engine)
